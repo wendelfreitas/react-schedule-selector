54481a5b0e6f97cc03f80144ce22deb15a26de83
Wendel Freitas <wendelfreitasbranco@gmail.com>
2020-09-28 15:16:02 -0300

    testing

---
+++ dist/lib/colors.js
@@ -0,0 +1,13 @@
+'use strict';
+
+exports.__esModule = true;
+var colors = {
+  blue: 'rgba(237, 28, 36, 1)',
+  lightBlue: 'rgba(162, 198, 248, 1)',
+  white: 'rgba(255, 255, 255, 1)',
+  black: 'rgba(79, 79, 79, 1)',
+  grey: 'rgba(79, 79, 79, 0.87)',
+  paleBlue: '#dbedff'
+};
+
+exports.default = colors;
